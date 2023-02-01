5db485f2b99d3bd0843c642d3a9df7d17c5534f3
myself <1907312518@qq.com>
2023-02-01 15:21:42 +0800

    Button 属性绑定细节

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,25 @@
+<script lang="ts">
+export default {
+  inheritAttrs: false
+};
+</script>
 <script setup lang="ts">
+import {useAttrs} from 'vue';
+
+const {size, ...rest} = useAttrs();
 </script>
 
+
 <template>
-  <button><slot/></button>
+  <div>
+    <button v-bind="rest">
+      <slot/>
+    </button>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
 </style>
--- src/views/ButtonDemo.vue
+++ src/views/ButtonDemo.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import Button from '../components/Button.vue'
+
+const onClick=()=>{
+  console.log('hi');
+}
 </script>
 
 <template>
   <div>Button 示例</div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
+    <Button size="大"  @click="onClick" @focus="onClick" @mouseover="onClick">你好</Button>
   </div>
 </template>
 
