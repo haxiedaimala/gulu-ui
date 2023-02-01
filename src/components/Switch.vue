f350fbe5b678862ee7e947affe12edbc16f30e71
myself <1907312518@qq.com>
2023-02-01 13:00:37 +0800

    Switch css 1

---
+++ src/components/Switch.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <button><span></span></button>
+</template>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: ($h)-4px;
+button {
+  width: $h*2;
+  height: $h;
+  border: none;
+  border-radius: $h/2;
+  background: blue;
+  position: relative;
+
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    width: $h2;
+    height: $h2;
+    background: #fff;
+    border-radius: $h2/2;
+  }
+}
+</style>
