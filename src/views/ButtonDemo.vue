1066fafb225dce2df6bf5735f3549d7b23ea3a9a
myself <1907312518@qq.com>
2023-02-01 14:32:11 +0800

    创建 Button 组件

--- src/views/ButtonDemo.vue
+++ src/views/ButtonDemo.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import Button from '../components/Button.vue'
 </script>
 
 <template>
-  <div>ButtonDemo</div>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
+  </div>
 </template>
 
 <style lang="scss" scoped>
