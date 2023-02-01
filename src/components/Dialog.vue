8b3de466c2a2e2f5beb687f57fefa286c83a104b
myself <1907312518@qq.com>
2023-02-01 23:31:35 +0800

    创建 Dialog 组件

---
+++ src/components/Dialog.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Button from './Button.vue'
+</script>
+
+<template>
+  <div class="gulu-dialog-overlay"></div>
+  <div class="gulu-dailog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>内容1</p>
+      <p>内容2</p>
+    </main>
+    <footer>
+      <Button>Ok</Button>
+      <Button>Cancle</Button>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
--- src/views/DialogDemo.vue
+++ src/views/DialogDemo.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import Dialog from '../components/Dialog.vue'
 </script>
 
 <template>
-  <div>DialogDemo</div>
+  <div>Dialog 示例</div>
+  <h2>示例1</h2>
+  <Dialog/>
 </template>
 
 <style lang="scss" scoped>
