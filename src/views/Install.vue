58e5bdc19fc572f18507e242808369bc241805c6
myself <1907312518@qq.com>
2023-02-03 23:30:51 +0800

    填充文档内容

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -2,7 +2,16 @@
 </script>
 
 <template>
-  <div>安装</div>
+  <article>
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install gulu-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add gulu-ui</code></pre>
+    <p>
+      下一节：<router-link to="/doc/get-started">开始使用</router-link>
+    </p>
+  </article>
 </template>
 
 <style lang="scss" scoped>
