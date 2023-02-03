6bcd7dad585b30a83916ea54b81f35537ef25ccb
myself <1907312518@qq.com>
2023-02-03 23:14:45 +0800

    复杂一点的高亮当前路由

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -72,20 +72,24 @@ const asideVisible = inject<Ref<boolean>>('asideVisible')!;
       position: fixed;
       top: 0;
       left: 0;
-      padding: 70px 16px 16px;
+      padding: 70px 0 16px;
       height: 100%;
 
 
       > h2 {
         margin-bottom: 4px;
+        padding: 0 16px;
       }
 
       > ol {
         > li {
-          padding: 4px 0;
+          a {
+            padding: 4px 16px;
+            display: block;
+          }
 
-          > .router-link-active {
-            text-decoration: underline;
+          .router-link-active {
+            background: white;
           }
         }
       }
