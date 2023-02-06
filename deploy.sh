9167d6553aebd0b4475316271caff68052890509
myself <1907312518@qq.com>
2023-02-06 22:33:52 +0800

    添加一键部署脚本

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M main &&
+git remote add origin git@github.com:haxiedaimala/vue-gulu-UI.git &&
+git push -f -u origin main &&
+cd -
+echo https://haxiedaimala.github.io/vue-gulu-UI/index.html
