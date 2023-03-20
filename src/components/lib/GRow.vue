b08951124d954d0df3b119cd5ad3c4c508ab918e
myself <1907312518@qq.com>
2023-03-20 13:57:51 +0800

    支持 gutter,每一列间隔多少

--- src/components/lib/GRow.vue
+++ src/components/lib/GRow.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
-import {useSlots} from 'vue';
+import {onMounted, ref, useSlots} from 'vue';
 import GCol from './GCol.vue';
 
+const props = defineProps({
+  gutter: {
+    type: Number,
+    default: 0,
+    validator: (value: number) => value >= 0
+  }
+});
 if (useSlots().default === undefined) throw new Error('GRow 子内容不能为空');
 useSlots().default!().forEach(item => {
   if (item.type !== GCol) throw new Error('Grow 的子元素必须是 GCol');
 });
+
+const rowItem = ref<HTMLDivElement>();
+onMounted(() => {
+  if (props.gutter < 0) return;
+  const cloList = rowItem.value!.children;
+  const length = cloList.length || 0;
+  for (let i = 0; i < length - 1; i++) {
+    (cloList[i] as HTMLDivElement).style.marginRight = props.gutter + 'px';
+  }
+});
 </script>
 
 <template>
-  <div class="gulu-layout-row">
+  <div class="gulu-layout-row" ref="rowItem">
     <slot/>
   </div>
 </template>
---
+++ src/sourceCode/layout/layout2.sourceCode.vue
@@ -0,0 +1,49 @@
+<sourceCode>
+分栏间隔
+<detail>
+  <p>支持列间距。</p>
+  <br/>
+  行提供 <code>gutter</code> 属性来指定列之间的间距，其默认值为0。
+</detail>
+</sourceCode>
+
+<script setup lang="ts">
+import GRow from '../../components/lib/GRow.vue';
+import GCol from '../../components/lib/GCol.vue';
+</script>
+
+<template>
+  <g-row :gutter="20">
+    <g-col :span="6"><div class="grid-content grid-bg-light"/></g-col>
+    <g-col :span="6"><div class="grid-content gird-bg"/></g-col>
+    <g-col :span="6"><div class="grid-content grid-bg-light"/></g-col>
+    <g-col :span="6"><div class="grid-content gird-bg"/></g-col>
+  </g-row>
+</template>
+
+<style lang="scss">
+.gulu-layout-row {
+  margin-bottom: 20px;
+}
+
+.gulu-layout-row:last-child {
+  margin-bottom: 0;
+}
+
+.gulu-layout-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.grid-bg-light {
+  background-color: #e5e9f2;
+}
+
+.gird-bg {
+  background-color: #d3dce6;
+}
+</style>
