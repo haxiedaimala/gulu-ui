f35e4b3ac3fd14aa7913747662f1a89e7dfad2eb
myself <1907312518@qq.com>
2023-03-23 09:58:52 +0800

    支持内容的属性传值和 slot 书写两种方式

--- src/components/lib/Popover.vue
+++ src/components/lib/Popover.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
+defineProps({
+  content: {
+    type: String
+  }
+});
 </script>
 
 <template>
-  <div>POPOVER</div>
+  <div class="gulu-popover">
+    <div class="gulu-popover-content">
+      <div v-if="content">{{ content }}</div>
+      <template v-else>
+        <slot/>
+      </template>
+    </div>
+    <div class="gulu-popover-action">
+      <slot name="reference"/>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.gulu-popover {
+  display:inline-flex;
+  &-content {
+
+  }
+
+  &-action {
+
+  }
+}
 </style>
--- src/sourceCode/propover/popover1.sourceCode.vue
+++ src/sourceCode/propover/popover1.sourceCode.vue
@@ -2,14 +2,27 @@
 常规用法
 <detail>
   <code>trigger</code> 属性被用来决定 <code>popover</code> 的触发方式，
-  支持的触发方式： <code>hover</code>、<code>click</code>、<code>focus</code> 或 <code>contextmenu</code>。 如果你想手动控制它，可以设置 <code>:visible</code> 属性。
+  支持的触发方式： <code>hover</code>、<code>click</code>、<code>focus</code> 或 <code>contextmenu</code>。 如果你想手动控制它，可以设置 <code>:visible</code>
+  属性。
 </detail>
 </sourceCode>
 
 <script setup lang="ts">
-import Popover from '../../components/lib/Popover.vue'
+import Popover from '../../components/lib/Popover.vue';
+import Button from '../../components/lib/Button.vue';
 </script>
 
 <template>
-  <Popover></Popover>
+  <Popover content="this is content,this is content,this is content">
+    <template #reference>
+      <Button>Hover to activate</Button>
+    </template>
+  </Popover>
+
+  <Popover>
+    <span>this is content,this is content,this is content</span>
+    <template #reference>
+      <Button>Hover to activate</Button>
+    </template>
+  </Popover>
 </template>
